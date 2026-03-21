ec11851649381b43abc72c4bc4f617fa0d9d3f06
Jana Saad <janaahmedsaad@gmail.com>
2026-03-21 19:15:38 +0200

    empty charts components

---
+++ src/app/components/bar-chart/bar-chart.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bar-chart',
+  imports: [],
+  templateUrl: './bar-chart.html',
+  styleUrl: './bar-chart.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class BarChartComponent {}
---
+++ src/app/components/doughnut-chart/doughnut-chart.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-doughnut-chart',
+  imports: [],
+  templateUrl: './doughnut-chart.html',
+  styleUrl: './doughnut-chart.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DoughnutChartComponent {}
